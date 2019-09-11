6653c4045817e0513a34c6d1ddf6c5b56f125819
Brady Douthit <brady.douthit99@gmail.com>
2019-09-11 14:19:27 -0400

    added a backdrop for when the sideDeawer is open

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React from 'react';
 
 import Toolbar from './components/Toolbar/Toolbar'
 import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
 
+class App extends React.Component  {
+  state = {
+    SideDrawerOpen: false
+  }
+  drawerToggleClickHandler = () =>{
+    this.setState((prevState) => {
+      return {SideDrawerOpen: !prevState.SideDrawerOpen}
+    })
+  };
+  render() {
+    return (
+      <div className="App" style={{height: "100%"}}>
+        <Toolbar />
+        <SideDrawer />
+        <Backdrop />
+        <main style={{marginTop: '64px'}}>
+          <p>This is the main content</p>
+        </main>
+      </div>
+    );
+  }
 
-function App() {
-  return (
-    <div className="App" style={{height: "100%"}}>
-      <Toolbar />
-      <SideDrawer />
-      <main style={{marginTop: '64px'}}>
-        <p>This is the main content</p>
-      </main>
-    </div>
-  );
 }
 
 export default App;
