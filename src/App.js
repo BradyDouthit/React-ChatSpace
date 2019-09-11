f9ff8ed77c645b7484f7d8f50d011f0510f35b55
Brady Douthit <brady.douthit99@gmail.com>
2019-09-11 14:36:39 -0400

    hamburger menu now functions

--- src/App.js
+++ src/App.js
@@ -4,22 +4,35 @@ import Toolbar from './components/Toolbar/Toolbar'
 import SideDrawer from './components/SideDrawer/SideDrawer';
 import Backdrop from './components/Backdrop/Backdrop';
 
-class App extends React.Component  {
+class App extends React.Component {
   state = {
-    SideDrawerOpen: false
+    sideDrawerOpen: false
   }
-  drawerToggleClickHandler = () =>{
+
+  drawerToggleClickHandler = () => {
     this.setState((prevState) => {
-      return {SideDrawerOpen: !prevState.SideDrawerOpen}
+      return { sideDrawerOpen: !prevState.sideDrawerOpen }
     })
   };
+
+  backdropClickHandler = () => {
+    this.setState({sideDrawerOpen: false})
+  };
+
   render() {
+    let sideDrawer;
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      sideDrawer = <SideDrawer />;
+      backdrop = <Backdrop click={this.drawerToggleClickHandler} />
+    }
     return (
-      <div className="App" style={{height: "100%"}}>
-        <Toolbar />
-        <SideDrawer />
-        <Backdrop />
-        <main style={{marginTop: '64px'}}>
+      <div className="App" style={{ height: "100%" }}>
+        <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
+        {sideDrawer}
+        {backdrop}
+        <main style={{ marginTop: '64px' }}>
           <p>This is the main content</p>
         </main>
       </div>
